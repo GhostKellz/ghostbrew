6b66e6a60dcc887f1c5ff6ea3be0684ef0476975
GhostKellz <ckelley@ghostkellz.sh>
2025-05-17 18:17:58 -0400

    Add main.go entry point

--- main.go
+++ main.go
@@ -1,9 +1,8 @@
-/*
-Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-*/
 package main
 
-import "github.com/ghostkellz/ghostbrew/cmd"
+import (
+	"github.com/ghostkellz/ghostbrew/cmd"
+)
 
 func main() {
 	cmd.Execute()
